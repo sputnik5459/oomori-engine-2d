c72b68670542ca6c7163ee92c7420a5c6892cd25
sputnik5459 <sputnik5459@gmail.com>
2021-08-06 18:24:12 +0300

    add Resources, Render refactor

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+#include "Renderer/ShaderProgram.h"
+#include "Resources/ResourceManager.h"
+
 
 GLfloat point[] = {
    0.0f,  0.5f, 0.0f,
@@ -48,7 +51,7 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
   }
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
     GLFWwindow* pWindow;
 
@@ -91,56 +94,47 @@ int main(void)
 
     glClearColor(0, 1, 0 ,1);
 
-    // Perform shaders
 
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, nullptr);
-    glCompileShader(vs);
+    { // new namespace in order to use destructor of ResourceManager
+      ResourceManager resourceManager(argv[0]);
+      auto pDefaultShaderProgram = resourceManager.loadShaders(
+        "DefaultShader", "res/shaders/vertex.txt", "res/shaders/fragment.txt"
+      );
+      if (!pDefaultShaderProgram)
+      {
+        std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+        return -1;
+      }
 
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragment_shader, nullptr);
-    glCompileShader(fs);
+      GLuint points_vbo = 0;
+      glGenBuffers(1, &points_vbo);
+      glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+      glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW); // from RAM to graphics card's memory
 
-    GLuint shader_program_id = glCreateProgram();
-    glAttachShader(shader_program_id, vs);
-    glAttachShader(shader_program_id, fs);
-    glLinkProgram(shader_program_id); // link shaders
+      GLuint colors_vbo = 0;
+      glGenBuffers(1, &colors_vbo);
+      glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+      glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW); // from RAM to graphics card's memory
 
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+      GLuint vao = 0;
+      glGenVertexArrays(1, &vao);
+      glBindVertexArray(vao);
 
-    // Send to videocard (using VBO - https://ru.wikipedia.org/wiki/Vertex_Buffer_Object)
+      glEnableVertexAttribArray(0);
+      glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-    GLuint points_vbo = 0;
-    glGenBuffers(1, &points_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW); // from RAM to graphics card's memory
+      glEnableVertexAttribArray(1);
+      glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-
-    GLuint colors_vbo = 0;
-    glGenBuffers(1, &colors_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW); // from RAM to graphics card's memory
-
-    GLuint vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(pWindow))
-    {
+      /* Loop until the user closes the window */
+      while (!glfwWindowShouldClose(pWindow))
+      {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program_id);
+        pDefaultShaderProgram->use();
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -150,7 +144,9 @@ int main(void)
 
         /* Poll for and process events */
         glfwPollEvents();
+      }
     }
+
 
     // free all resources
     glfwTerminate();
